e16ae91d8c4fb5cec9a7072ef75dda49d7b41728
chakir qatab <javachakir@gmail.com>
2023-02-21 14:00:11 +0400

    create vite project with storybook init, template

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -6,34 +6,43 @@ export default {
   component: MyButton,
   tags: ['autodocs'],
   argTypes: {
+    backgroundColor: {
+      control: 'color',
+    },
+    onClick: {},
     size: {
       control: {
         type: 'select',
       },
       options: ['small', 'medium', 'large'],
     },
-    variant: {
-      control: {
-        type: 'select',
-      },
-      options: ['primary', 'secondary'],
-    }
   },
 };
 
 // More on writing stories with args: https://storybook.js.org/docs/7.0/vue/writing-stories/args
 export const Primary = {
-  story: (args) => ({
-    // same with v-bind
-    // template: `<my-button variant="primary" v-bind="args">{{args.default}}</my-button>`,
-    template: `<my-button variant="primary">{{args.default}}</my-button>`,
-    setup() {
-      return {
-        args,
-      }
-    }, 
-  }),
   args: {
-    default: 'Button!'
-  }
+    primary: true,
+    label: 'Button',
+  },
+};
+
+export const Secondary = {
+  args: {
+    label: 'Button',
+  },
+};
+
+export const Large = {
+  args: {
+    size: 'large',
+    label: 'Button',
+  },
+};
+
+export const Small = {
+  args: {
+    size: 'small',
+    label: 'Button',
+  },
 };
---
+++ src/stories/kevin/Button.stories.js
@@ -0,0 +1,39 @@
+import MyButton from './Button.vue';
+
+// More on how to set up stories at: https://storybook.js.org/docs/7.0/vue/writing-stories/introduction
+export default {
+  title: 'Example/Kevin/Button',
+  component: MyButton,
+  tags: ['autodocs'],
+  argTypes: {
+    size: {
+      control: {
+        type: 'select',
+      },
+      options: ['small', 'medium', 'large'],
+    },
+    variant: {
+      control: {
+        type: 'select',
+      },
+      options: ['primary', 'secondary'],
+    }
+  },
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/7.0/vue/writing-stories/args
+export const PrimaryStory = {
+  story: (args) => ({
+    // same with v-bind
+    // template: `<my-button variant="primary" v-bind="args">{{args.default}}</my-button>`,
+    template: `<my-button variant="primary">{{args.default}}</my-button>`,
+    setup() {
+      return {
+        args,
+      }
+    }, 
+  }),
+  args: {
+    default: 'Button!'
+  }
+};
